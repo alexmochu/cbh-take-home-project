aba1206cbe9f110bd044108431f0a16b299cfdcb
alexmochu <mochualex4@gmail.com>
2023-01-10 14:09:53 +0300

    add tests

--- dpk.test.js
+++ dpk.test.js
@@ -5,4 +5,12 @@ describe("deterministicPartitionKey", () => {
     const trivialKey = deterministicPartitionKey();
     expect(trivialKey).toBe("0");
   });
+  it("Should still return '0' even if partitionKey exists", () => {
+    const event = { partitionKey: "abc" };
+    expect(deterministicPartitionKey(event)).toEqual("0");
+  });
+  it("Should still return '0' even if the sha3-512 hash exists and partitionKey doesn't", () => {
+    const event = { data: { name: "John" } };
+    expect(deterministicPartitionKey(event)).toEqual("0");
+  });
 });
